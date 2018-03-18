5273f01259c805171268a76ee3ae4a8588a39be8
aleksei <alekseiosh@gmail.ru>
2018-03-18 13:36:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+WalkMyAndroid - Starter Code
+============
+
+Starter code for the Walk My Android app which tracks location using the
+Location API.
+
+Introduction
+------------
+The starter code includes two layout files, one for landscape and one for
+portrait mode, some images, and a predefined animation that will be used to
+build up the final version.
+
+
+Pre-requisites
+--------------
+
+You should be familiar with:
+- Creating, building, and running apps in Android Studio.
+- The Activity lifecycle.
+- Persisting data across configuration changes.
+- How to use an AsyncTask to do background work.
+- Requesting permissions at runtime.
---
+++ app/src/main/java/com/example/android/walkmyandroid/MainActivity.java
@@ -0,0 +1,26 @@
+
+package com.example.android.walkmyandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mLocationButton;
+    private TextView mLocationTextView;
+    private ImageView mAndroidImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLocationButton = (Button) findViewById(R.id.button_location);
+        mLocationTextView = (TextView) findViewById(R.id.textview_location);
+        mAndroidImageView = (ImageView) findViewById(R.id.imageview_android);
+
+    }
+}
